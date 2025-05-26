1cfbdd1ed2e50cfa50e0c94e4f74645a153ce0d5
joshuawuro <joshuawuro@gmail.com>
2025-05-26 14:50:54 +0000

    project file setup

---
+++ crudapp/src/App.tsx
@@ -0,0 +1,11 @@
+import Crud from "./component/crud";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
+      <Crud />
+    </div>
+  );
+}
+
+export default App;
